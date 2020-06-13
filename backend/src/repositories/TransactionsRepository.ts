cc579fc1060f2678db2f45912c847797df4035e4
Gabriel Pereira <gabriel9938@gmail.com>
2020-06-13 12:40:28 -0300

    Initial challange

--- backend/src/repositories/TransactionsRepository.ts
+++ backend/src/repositories/TransactionsRepository.ts
@@ -1,3 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -6,39 +8,23 @@ interface Balance {
   total: number;
 }
 
-interface CreateTransactionDTO {
-  title: string;
-  value: number;
-  type: 'income' | 'outcome';
-}
-
-class TransactionsRepository {
-  private transactions: Transaction[];
-
-  constructor() {
-    this.transactions = [];
-  }
-
-  public all(): Transaction[] {
-    return this.transactions;
-  }
-
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
   public getBalance(): Balance {
-    const {income, outcome } = this.transactions.reduce(
+    const { income, outcome } = this.transactions.reduce(
       (accumulator: Balance, transaction: Transaction) => {
-
-      switch (transaction.type){
-          case "income":
+        switch (transaction.type) {
+          case 'income':
             accumulator.income += transaction.value;
             break;
-          case "outcome":
+          case 'outcome':
             accumulator.outcome += transaction.value;
             break;
           default:
             break;
         }
 
-        return accumulator
+        return accumulator;
       },
       {
         income: 0,
@@ -47,17 +33,9 @@ class TransactionsRepository {
       },
     );
 
-    const total = income - outcome
+    const total = income - outcome;
 
-    return { income, outcome , total};
-  }
-
-  public create({ title, value, type }: CreateTransactionDTO): Transaction {
-    const transaction = new Transaction({ title, value, type });
-
-    this.transactions.push(transaction);
-
-    return transaction;
+    return { income, outcome, total };
   }
 }
 
--- backend/src/services/CreateTransactionService.ts
+++ backend/src/services/CreateTransactionService.ts
@@ -1,33 +1,10 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
+// import AppError from '../errors/AppError';
+
 import Transaction from '../models/Transaction';
 
-interface Request {
-  title: string;
-  value: number;
-  type: 'income' | 'outcome';
-}
-
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
-
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
-
-  public execute({ title, value, type }: Request): Transaction {
-    const balance = this.transactionsRepository.getBalance();
-
-    if (balance.total < value && type === 'outcome') {
-      throw Error('The amount withdrawn is greater than the total balance.');
-    }
-
-    const appointment = this.transactionsRepository.create({
-      title,
-      value,
-      type,
-    });
-
-    return appointment;
+  public async execute(): Promise<Transaction> {
+    // TODO
   }
 }
 
